c59cf7258a5d729df8853ee2f0438b6f2ca73c15
patti mint <patseade@Pats-MacBook-Pro.local>
2018-08-15 19:05:51 -0500

    second

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,10 @@ const words = ['Dennis', 'Dee', 'Charlie', 'Mac', 'McDonald', 'Frank', 'Cricket'
 // choose word randomely
 let ranNumber = Math.floor(Math.random() * words.length);
 let wordChosen = words[ranNumber];
+let rightWord = [];
+let wrongWord = [];
 let underScore = [];
+
 console.log(wordChosen)
 // create underscores based on words
 
@@ -18,7 +21,17 @@ console.log(generateUnderscore());
 
 // guess word
 document.addEventListener('keypress', (event) => {
-    console.log(event);
+    let keyword = String.fromCharCode(event.keyCode);
+// if guessed right
+    if (wordChosen.indexOf(keyword) > -1) {
+        console.log(true);
+    }
+    // right word array
+    rightWord.push(keyword);
+    console.log(rightWord);
+
+    wrongWord.push(keyword);
+    console.log(wrongWord);
 });
 // check if guess is correct
 // if correct push to correct array
